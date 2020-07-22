eb0b135fd331d7ac07f11ba04640f96cbaff4be0
Loh Siu Yin <siuyin@beyondbroadcast.com>
2020-07-22 16:53:50 +0800

    Refactored to UpdateLEDTask.

--- Sources/main.c
+++ Sources/main.c
@@ -42,7 +42,7 @@
 #include "ADC_PDD.h"
 #include "interrupts.h"
 
-unsigned int result;
+unsigned int ADCResult;
 void SenseADCInputTask(void) {
 	static unsigned int nrt;
 	if (tick != nrt) {
@@ -53,13 +53,25 @@ void SenseADCInputTask(void) {
 	ADC_PDD_WriteStatusControl1Reg(ADC0_BASE_PTR, 0, ADC_PDD_SINGLE_ENDED_AD6);	// start conversion
 	while (!ADC_PDD_GetConversionCompleteFlag(ADC0_BASE_PTR, 0))
 		;	// wait for conversion to complete
-	result = ADC_PDD_GetResultValueRaw(ADC0_BASE_PTR, 0);
-	if (result > 2047) {
+	ADCResult = ADC_PDD_GetResultValueRaw(ADC0_BASE_PTR, 0);
+
+}
+
+void UpdateLEDTask(void) {
+	static unsigned int nrt; // next run tick
+	if (tick != nrt) {
+		return;
+	}
+	nrt += 20;
+
+	if (ADCResult > 2047) {
 		GPIO_PDD_ClearPortDataOutputMask(GPIOB_BASE_PTR, GPIO_PDD_PIN_8);
 	} else {
 		GPIO_PDD_SetPortDataOutputMask(GPIOB_BASE_PTR, GPIO_PDD_PIN_8);
 	}
+
 }
+
 /*lint -save  -e970 Disable MISRA rule (6.3) checking. */
 int main(void)
 /*lint -restore Enable MISRA rule (6.3) checking. */
@@ -73,20 +85,21 @@ int main(void)
 	/* Write your code here */
 
 	// GPIO_PDD_ClearPortDataOutputMask(GPIOB_BASE_PTR,GPIO_PDD_PIN_8);	// turn on red LED
-
 	while (1) {
 		SenseADCInputTask();
+		UpdateLEDTask();
 	}
 
 	/*** Don't write any code pass this line, or it will be deleted during code generation. ***/
-  /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
-  #ifdef PEX_RTOS_START
-    PEX_RTOS_START();                  /* Startup of the selected RTOS. Macro is defined by the RTOS component. */
-  #endif
-  /*** End of RTOS startup code.  ***/
-  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
-  for(;;){}
-  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+	/*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
+#ifdef PEX_RTOS_START
+	PEX_RTOS_START(); /* Startup of the selected RTOS. Macro is defined by the RTOS component. */
+#endif
+	/*** End of RTOS startup code.  ***/
+	/*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+	for (;;) {
+	}
+	/*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
 } /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
 
 /* END main */
